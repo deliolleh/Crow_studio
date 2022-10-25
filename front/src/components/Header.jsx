d2efcabeed4013bfef12249519f0f51f7cce7d8e
foursoob <foursoob@gmail.com>
2022-10-25 17:44:50 +0900

    S07P31D207-191 design: 유저 정보 셋팅

--- front/src/components/Header.jsx
+++ front/src/components/Header.jsx
@@ -1,11 +1,12 @@
 import React from "react";
+import {Link} from "react-router-dom";
 
 export default function Header({ fixed }) {
   const [navbarOpen, setNavbarOpen] = React.useState(false);
   return (
     <>
-      <nav className="relative flex flex-wrap items-center justify-between px-2 py-3 bg-component_item_bg_dark m-3 rounded-lg">
-        <div className="container px-4 flex flex-wrap items-center justify-between">
+      <nav className="flex flex-wrap items-center justify-between px-2 py-3 bg-component_item_bg_dark m-3 rounded-lg">
+        <div className="container relative px-4 flex items-center justify-between">
           <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
             <div className="flex items-center">
               <div className="text-point_purple pr-3">Logo</div>
@@ -17,7 +18,7 @@ export default function Header({ fixed }) {
               </a>
             </div>
             <button
-              className="text-white cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
+              className="text-white cursor-pointer text-xl leading-none py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
               type="button"
               onClick={() => setNavbarOpen(!navbarOpen)}
             >
@@ -26,7 +27,7 @@ export default function Header({ fixed }) {
           </div>
           <div
             className={
-              "lg:flex flex-grow items-center justify-end" +
+              "lg:flex flex-grow items-center lg:justify-start justify-end" +
               (navbarOpen ? " flex" : " hidden")
             }
             id="example-navbar-danger"
@@ -34,15 +35,15 @@ export default function Header({ fixed }) {
             <ul className="flex flex-col lg:flex-row list-none">
               <li className="nav-item">
                 <a
-                  className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
-                  href="#pablo"
+                  className="px-0.5 py-2 flex items-center justify-end text-xs font-bold leading-snug text-white hover:opacity-75"
+                  href=""
                 >
                   <span className="ml-2">홈</span>
                 </a>
               </li>
               <li className="nav-item">
                 <a
-                  className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
+                  className="px-0.5 py-2 flex items-center justify-end text-xs font-bold leading-snug text-white hover:opacity-75"
                   href="#pablo"
                 >
                   <span className="ml-2">포럼</span>
@@ -50,13 +51,16 @@ export default function Header({ fixed }) {
               </li>
               <li className="nav-item">
                 <a
-                  className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
+                  className="px-0.5 py-2 flex items-center justify-end text-xs font-bold leading-snug text-white hover:opacity-75"
                   href="#pablo"
                 >
                   <span className="ml-2">프로젝트</span>
                 </a>
               </li>
             </ul>
+          </div>
+          <div className="lg:flex lg:static absolute right-12" >
+            <img className="w-7 rounded-full" src={require('../assets/images/avatar.png')} />
           </div>
         </div>
       </nav>
