725558c520fa048910ba8040cc846f7ec73bb992
박자연 <hosan130@yu.ac.kr>
2022-10-28 10:58:00 +0900

    S07P31D207-148 feat-be: JWT 서비스 작성

--- goldenCrow/src/main/java/com/example/goldencrow/user/UserService.java
+++ goldenCrow/src/main/java/com/example/goldencrow/user/UserService.java
@@ -1,7 +1,19 @@
 package com.example.goldencrow.user;
 
+import com.example.goldencrow.user.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    // 회원가입
+
+
+
+
+
 }
