b63adf2ee53226e2a7e6a40a2f87741e4d04bdba
박자연 <hosan130@yu.ac.kr>
2022-10-31 10:44:48 +0900

    S07P31D207-000 fix-be: 인터셉터 & jwt 로직 등록

--- back_file/src/main/java/com/ssafy/back_file/user/JwtService.java
+++ back_file/src/main/java/com/ssafy/back_file/user/JwtService.java
@@ -25,59 +25,59 @@ public class JwtService {
     private long accessTokenValidTime = Duration.ofDays(30).toMillis(); // 만료시간 30일
     private long refreshTokenValidTime = Duration.ofDays(100).toMillis(); // 만료시간 100일
 
-    // 액세스 토큰 만들기
-    public String createAccess(Long userSeq){
-
-        // 헤더 설정
-        Map<String, Object> headers = new HashMap<>();
-        headers.put("typ", "JWT");
-        headers.put("alg", "HS256");
-
-        // 페이로드 설정
-        Map<String, Object> payloads = new HashMap<>();
-        payloads.put("jti", userSeq);
-
-        Date ext = new Date();
-        ext.setTime(ext.getTime()+accessTokenValidTime);
-
-        // 토큰 빌드
-        String jwt = Jwts.builder()
-                .setHeader(headers)
-                .setClaims(payloads)
-                .setExpiration(ext)
-                .signWith(SignatureAlgorithm.HS256, SECRET_KEY.getBytes())
-                .compact();
-
-        return jwt;
-
-    }
-
-    // 리프레시 토큰 만들기
-    public String createRefresh(Long userSeq){
-
-        // 헤더 설정
-        Map<String, Object> headers = new HashMap<>();
-        headers.put("typ", "JWT");
-        headers.put("alg", "HS256");
-
-        // 페이로드 설정
-        Map<String, Object> payloads = new HashMap<>();
-        payloads.put("jti", userSeq);
-
-        Date ext = new Date();
-        ext.setTime(ext.getTime()+refreshTokenValidTime);
-
-        // 토큰 빌드
-        String jwt = Jwts.builder()
-                .setHeader(headers)
-                .setClaims(payloads)
-                .setExpiration(ext)
-                .signWith(SignatureAlgorithm.HS256, SECRET_KEY.getBytes())
-                .compact();
-
-        return jwt;
-
-    }
+//    // 액세스 토큰 만들기
+//    public String createAccess(Long userSeq){
+//
+//        // 헤더 설정
+//        Map<String, Object> headers = new HashMap<>();
+//        headers.put("typ", "JWT");
+//        headers.put("alg", "HS256");
+//
+//        // 페이로드 설정
+//        Map<String, Object> payloads = new HashMap<>();
+//        payloads.put("jti", userSeq);
+//
+//        Date ext = new Date();
+//        ext.setTime(ext.getTime()+accessTokenValidTime);
+//
+//        // 토큰 빌드
+//        String jwt = Jwts.builder()
+//                .setHeader(headers)
+//                .setClaims(payloads)
+//                .setExpiration(ext)
+//                .signWith(SignatureAlgorithm.HS256, SECRET_KEY.getBytes())
+//                .compact();
+//
+//        return jwt;
+//
+//    }
+//
+//    // 리프레시 토큰 만들기
+//    public String createRefresh(Long userSeq){
+//
+//        // 헤더 설정
+//        Map<String, Object> headers = new HashMap<>();
+//        headers.put("typ", "JWT");
+//        headers.put("alg", "HS256");
+//
+//        // 페이로드 설정
+//        Map<String, Object> payloads = new HashMap<>();
+//        payloads.put("jti", userSeq);
+//
+//        Date ext = new Date();
+//        ext.setTime(ext.getTime()+refreshTokenValidTime);
+//
+//        // 토큰 빌드
+//        String jwt = Jwts.builder()
+//                .setHeader(headers)
+//                .setClaims(payloads)
+//                .setExpiration(ext)
+//                .signWith(SignatureAlgorithm.HS256, SECRET_KEY.getBytes())
+//                .compact();
+//
+//        return jwt;
+//
+//    }
 
     // 이거 우리 토큰 맞는지, 유효기간 남았는지, 파스하면 뭔지 확인하기
     public Map<String, Object> verifyJWT(String jwt) throws Exception {
@@ -126,28 +126,28 @@ public class JwtService {
 
     }
 
-    // 리프레시 토큰을 보내주면 그걸로 새로 액세스 토큰을 발급하기
-    public String createRefreshByAccess(String jwt)throws UnsupportedEncodingException{
-        Map<String, Object> claimMap = new HashMap<>();
-
-        try {
-            Claims claims = Jwts.parser()
-                    .setSigningKey(SECRET_KEY.getBytes("UTF-8"))
-                    .parseClaimsJws(jwt)
-                    .getBody();
-
-            // 리프레시 토큰이 만료되었을 것이라는 전제는 없다...
-
-            claimMap = claims;
-            Long userSeq = (Long) claimMap.get("jti");
-
-            return createAccess(userSeq);
-
-        } catch (Exception e){
-            System.out.println(e);
-            return null;
-        }
-
-    }
+//    // 리프레시 토큰을 보내주면 그걸로 새로 액세스 토큰을 발급하기
+//    public String createRefreshByAccess(String jwt)throws UnsupportedEncodingException{
+//        Map<String, Object> claimMap = new HashMap<>();
+//
+//        try {
+//            Claims claims = Jwts.parser()
+//                    .setSigningKey(SECRET_KEY.getBytes("UTF-8"))
+//                    .parseClaimsJws(jwt)
+//                    .getBody();
+//
+//            // 리프레시 토큰이 만료되었을 것이라는 전제는 없다...
+//
+//            claimMap = claims;
+//            Long userSeq = (Long) claimMap.get("jti");
+//
+//            return createAccess(userSeq);
+//
+//        } catch (Exception e){
+//            System.out.println(e);
+//            return null;
+//        }
+//
+//    }
 
 }
