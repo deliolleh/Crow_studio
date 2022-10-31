facf658b0daa27ba346ee112d5f4f81a6de0ea6f
박자연 <hosan130@yu.ac.kr>
2022-10-31 14:30:25 +0900

    S07P31D207-152 fix-be: WebMvcConfig에 @Configuration 추가

--- back_file/src/main/java/com/ssafy/back_file/config/WebMvcConfig.java
+++ back_file/src/main/java/com/ssafy/back_file/config/WebMvcConfig.java
@@ -1,10 +1,12 @@
 package com.ssafy.back_file.config;
 
 import com.ssafy.back_file.common.JwtInterceptor;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
 
     @Override
