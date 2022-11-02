302fea1a23561e60d277276bcfec56d56743398f
우영택 <do0134@naver.com>
2022-11-02 17:05:57 +0900

    fix-be : django starter 14-6

--- goldenCrow/src/main/java/com/example/goldencrow/file/Controller/ProjectController.java
+++ goldenCrow/src/main/java/com/example/goldencrow/file/Controller/ProjectController.java
@@ -73,9 +73,9 @@ public class ProjectController {
         ProcessBuilder tester = new ProcessBuilder();
         try {
             builder.command("ls");
-            builder.directory(new File("./"));
-//            tester.command("django-admin","startproject","helloworld");
-//            tester.directory(new File("./"));
+            builder.directory(new File("./home/ubuntu/crow_data/15"));
+            tester.command("django-admin","startproject","helloworld");
+            tester.directory(new File("./home/ubuntu/crow_data/15"));
         } catch (Exception e) {
             return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
         }
