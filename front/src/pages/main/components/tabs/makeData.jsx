b0fa714d4f1f8f13b219a46e516777cf728f7c9c
JaeHyuk Han <jhhan2206@naver.com>
2022-11-06 23:26:17 +0900

    feat-fe: editor forum api 등록

--- front/src/pages/main/components/tabs/makeData.jsx
+++ front/src/pages/main/components/tabs/makeData.jsx
@@ -3,21 +3,61 @@ import React, { useState, useEffect, useRef } from "react";
 import Editor from "@monaco-editor/react";
 import files from "./files";
 import ReactTerminal from "react-terminal-component";
-import { EmulatorState, FileSystem } from 'javascript-terminal';
+import { EmulatorState, FileSystem } from "javascript-terminal";
+import { useDispatch } from "react-redux";
+import editorApi from "../../../../api/editorApi";
 
-export const MakeEditorData = (number, titlePrefix = 'Tab', useTitleCounter = true) => {
-    // 에디터 파일
-    const editorRef = useRef(null);
-    const [fileName, setFileName] = useState("script.js");
-  
-    const file = files[fileName];
-  
-    useEffect(() => {
-      editorRef.current?.focus();
-    }, [file.name]);
+export const MakeEditorData = (
+  number,
+  titlePrefix = "Tab",
+  useTitleCounter = true
+) => {
+  const dispatch = useDispatch();
+
+  // 에디터 파일
+  const editorRef = useRef(null);
+  const [fileName, setFileName] = useState("script.js");
+  const [language, setLanguage] = useState("");
+
+  const file = files[fileName];
+
+  useEffect(() => {
+    editorRef.current?.focus();
+  }, [file.name]);
 
   const data = [];
 
+  // 포맷팅 영역
+  const format = () => {
+    editorApi.formatPut(language, editorRef).then((num) => {
+      editorApi
+        .formatGet(num)
+        .then((res) => {
+          editorRef = res;
+        })
+        .catch((err) => console.error(err));
+    });
+  };
+
+  // 린트 영역
+  const lint = () => {
+    editorApi
+      .lint(language, editorRef)
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  };
+
+  // 변수명 추천
+  const varRecommend = (letter) => {
+    editorApi
+      .variableRecommend(letter)
+      .then((res) => console.log(res))
+      .catch((err) => console.error(err));
+  };
 
   for (let i = 0; i < number; i++) {
     data.push({
@@ -32,13 +72,9 @@ export const MakeEditorData = (number, titlePrefix = 'Tab', useTitleCounter = tr
             height="calc(100vh - 31px)"
             theme="vs-dark"
             // path={file.name}
-            path={
-              i + 1 === 1 ? "script.js" : "style.css"
-            }
+            path={i + 1 === 1 ? "script.js" : "style.css"}
             // defaultLanguage={file.language}
-            defaultLanguage={
-              i + 1 === 1 ? "javascript" : "css"
-            }
+            defaultLanguage={i + 1 === 1 ? "javascript" : "css"}
             // defaultValue={file.value}
             defaultValue={
               i + 1 === 1 ? files["script.js"].value : files["style.css"].value
@@ -57,16 +93,20 @@ export const MakeEditorData = (number, titlePrefix = 'Tab', useTitleCounter = tr
   return data;
 };
 
-export const MakeConsoleData = (number, titlePrefix = 'Tab', useTitleCounter = true) => {
+export const MakeConsoleData = (
+  number,
+  titlePrefix = "Tab",
+  useTitleCounter = true
+) => {
   const data = [];
 
   const customState = EmulatorState.create({
-    'fs': FileSystem.create({
-      '/home': { },
-      '/home/README': {content: 'This is a text file'},
-      '/home/nested/directory': {},
-      '/home/nested/directory/file': {content: 'End of nested directory!'}
-    })
+    fs: FileSystem.create({
+      "/home": {},
+      "/home/README": { content: "This is a text file" },
+      "/home/nested/directory": {},
+      "/home/nested/directory/file": { content: "End of nested directory!" },
+    }),
   });
 
   for (let i = 0; i < number; i++) {
@@ -77,17 +117,17 @@ export const MakeConsoleData = (number, titlePrefix = 'Tab', useTitleCounter = t
           {/* <b>Content {i + 1}</b>
           <p>{lorem.generateWords(15)}</p> */}
           <ReactTerminal
-            promptSymbol='🔥>'
+            promptSymbol="🔥>"
             theme={{
-              background: '#333333',
-              promptSymbolColor: '#6effe6',
-              commandColor: '#fcfcfc',
-              outputColor: '#fcfcfc',
-              errorOutputColor: '#ff89bd',
-              fontSize: '14px',
-              spacing: '1.5%',
-              width: '100%',
-              height: 'calc(100vh - 31px)',
+              background: "#333333",
+              promptSymbolColor: "#6effe6",
+              commandColor: "#fcfcfc",
+              outputColor: "#fcfcfc",
+              errorOutputColor: "#ff89bd",
+              fontSize: "14px",
+              spacing: "1.5%",
+              width: "100%",
+              height: "calc(100vh - 31px)",
             }}
             emulatorState={customState}
           />
