a8e7bba48707df3ec7b89e73408a033d45967237
우영택 <do0134@naver.com>
2022-11-08 00:19:25 +0900

    feat-be : git add, commit, push 구현

--- goldenCrow/src/main/java/com/example/goldencrow/git/GitController.java
+++ goldenCrow/src/main/java/com/example/goldencrow/git/GitController.java
@@ -40,12 +40,35 @@ public class GitController {
         return "클론에 성공했습니다";
     }
 
-    @PostMapping("/{teamSeq}/branch")
+    @PostMapping("/{teamSeq}/git-switch")
     public String gitSwitch (@RequestHeader("Authorization") String jwt, @PathVariable Long teamSeq, @RequestParam Integer type, @RequestBody HashMap<String, String> gitProject) {
         String switchResult = gitService.gitSwitch(teamSeq,gitProject.get("projectName"),gitProject.get("branchName"),type);
         if (!switchResult.equals("Success")) {
             return switchResult;
         }
         return "깃 스위치에 성공했습니다!";
+    }
+
+    @PostMapping("/{teamSeq}/git-commit")
+    public String gitCommit(@RequestHeader("Authorization") String jwt, @RequestBody HashMap<String, String> gitFile) {
+        String message = gitFile.get("message");
+        String gitPath = gitFile.get("gitPath");
+        String filePath = gitFile.get("filePath");
+        String commitResult = gitService.gitCommit(message,gitPath,filePath);
+
+        return commitResult;
+    }
+
+    @PostMapping("/{userSeq}/git-push")
+    public String gitPush(@RequestHeader("Authorization") String jwt,@PathVariable Long userSeq, @RequestBody HashMap<String, String> gitFile) {
+        String message = gitFile.get("message");
+        String gitPath = gitFile.get("gitPath");
+        String filePath = gitFile.get("filePath");
+        String branchName = gitFile.get("branchName");
+        String email = gitFile.get("email");
+        String pass = gitFile.get("pass")
+        String pushCheck = gitService.gitPush(branchName,message,gitPath,filePath,userSeq,email,pass);
+
+        return pushCheck;
     }
 }
