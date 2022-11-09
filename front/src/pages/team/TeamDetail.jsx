554943c71b4ad91bf98a3fe4d8cfbc56dbdc7676
함희주 <gkagmlwn94@naver.com>
2022-11-09 17:12:43 +0900

    S07P31D207-342 feat-fe/team: 컴포넌트 분리, 디자인 적용

--- front/src/pages/team/TeamDetail.jsx
+++ front/src/pages/team/TeamDetail.jsx
@@ -28,6 +28,7 @@ const TeamDetail = () => {
   } = team;
 
   const [isEdit, setIsEdit] = useState(false);
+  const [isSearch, setIsSearch] = useState(false);
 
   const [inputTeamName, setInputTeamName] = useState(teamName);
 
@@ -101,6 +102,15 @@ const TeamDetail = () => {
       .then((res) => {
         alert(`${addUserName}님을 팀원으로 추가했습니다`);
         console.log(res);
+        setIsSearch(false);
+        setSearchResults([]);
+        dispatch(getTeam(teamSeq))
+          .unwrap()
+          .then((res) => {
+            setTeam(res);
+            console.log("res:", res);
+          })
+          .catch(console.error);
       })
       .catch((errorStatusCode) => {
         if (errorStatusCode === 409) {
@@ -119,7 +129,15 @@ const TeamDetail = () => {
     console.log("deleteData:", deleteData);
     dispatch(deleteMember(deleteData))
       .unwrap()
-      .then(console.log)
+      .then(() => {
+        dispatch(getTeam(teamSeq))
+          .unwrap()
+          .then((res) => {
+            setTeam(res);
+            console.log("res:", res);
+          })
+          .catch(console.error);
+      })
       .catch((errorStatusCode) => {
         console.error(errorStatusCode);
       });
@@ -128,6 +146,10 @@ const TeamDetail = () => {
   const openTeamNameEditorHandler = () => setIsEdit(true);
 
   const closeTeamNameEditorHandler = () => setIsEdit(false);
+
+  const openSearchInputHandler = () => setIsSearch(true);
+
+  const closeSearchInputHandler = () => setIsSearch(false);
 
   return (
     <div>
@@ -141,7 +163,7 @@ const TeamDetail = () => {
                 className="text-sm cursor-pointer"
                 onClick={openTeamNameEditorHandler}
               >
-                (편집)
+                ✏
               </span>
             </h1>
           )}
@@ -161,15 +183,27 @@ const TeamDetail = () => {
                 onClick={closeTeamNameEditorHandler}
                 className="cursor-pointer"
               >
-                X
+                ❌
               </span>
             </div>
           )}
 
           {teamLeaderSeq === mySeq && (
-            <button className="px-2 py-1 text-lg font-bold text-component_dark bg-point_pink hover:bg-point_red hover:text-white rounded-md transition">
-              팀 삭제
-            </button>
+            <div className="flex gap-2">
+              <button
+                onClick={clickTeamListHandler}
+                className="px-2 py-1 text-lg text-primary_dark bg-component_item_bg_dark border border-primary_-2_dark rounded-md"
+              >
+                팀 목록
+              </button>
+
+              <button
+                onClick={deleteTeamHandler}
+                className="px-2 py-1 text-lg font-bold text-component_dark bg-point_pink hover:bg-point_red hover:text-white rounded-md transition"
+              >
+                팀 삭제
+              </button>
+            </div>
           )}
 
           {!(teamLeaderSeq === mySeq) && (
@@ -180,63 +214,104 @@ const TeamDetail = () => {
         </div>
         <span className="text-point_light_yellow">{myNickname}</span>
         {/* <TeamList clickTeamDetail={clickTeamDetailHandler} teams={teams} /> */}
-      </div>
 
-      <div>팀장: {teamLeaderNickname}</div>
-      <div>
-        팀원:
-        {members?.map((member) => (
-          <div key={member.memberSeq}>
-            <div>
-              {member.memberNickname}{" "}
-              <span
-                onClick={() =>
-                  deleteMemberHandler(member.memberNickname, member.memberSeq)
-                }
-                className="cursor-pointer"
-              >
-                X
-              </span>
+        <div className="flex items-center">
+          <div className="w-48 text-white font-bold bg-point_purple h-full p-2 flex items-center rounded-bl-md rounded-tl-md">
+            팀장
+          </div>
+          <div className="flex">
+            <div className="flex flex-col gap-2 items-center p-2">
+              {/* <div className="bg-point_light_yellow w-11 h-11 rounded-full"></div> */}
+              <div className="text-white text-sm">{teamLeaderNickname}</div>
             </div>
           </div>
-        ))}
-      </div>
+        </div>
 
-      <br />
-
-      <div>유저 검색</div>
-      <div>
-        <form onSubmit={submitSearchUser}>
-          <input
-            type="text"
-            name="searchUser"
-            id="searchUser"
-            onChange={searchUserChangeHandler}
-            value={searchUserName}
-          />
-        </form>
-      </div>
-
-      <div>검색 결과</div>
-      <div>
-        {searchResults?.map((user) => (
-          <div
-            key={user.userId}
-            className="hover:cursor-pointer"
-            onClick={() => addUserHandler(user.userSeq, user.userNickname)}
-          >
-            {user.userNickname}
+        <div className="flex items-center">
+          <div className="w-48 text-white font-bold bg-point_purple h-full p-2 flex items-center rounded-bl-md rounded-tl-md">
+            팀원
           </div>
-        ))}
+          <div className="flex">
+            {members?.map((member) => (
+              <div
+                key={`m${member.memberSeq}`}
+                className="flex flex-col gap-2 items-center p-2"
+              >
+                {/* <div className="bg-point_light_yellow w-11 h-11 rounded-full"></div> */}
+                <div className="text-white text-sm">
+                  {member.memberNickname}{" "}
+                  <span
+                    className="cursor-pointer"
+                    onClick={() =>
+                      deleteMemberHandler(
+                        member.memberNickname,
+                        member.memberSeq
+                      )
+                    }
+                  >
+                    ❌
+                  </span>
+                </div>
+              </div>
+            ))}
+
+            <div className="flex flex-col gap-2 items-center p-2">
+              {/* <div className="bg-point_light_yellow w-11 h-11 rounded-full"></div> */}
+              {!isSearch && (
+                <div
+                  className="text-white text-sm cursor-pointer"
+                  onClick={openSearchInputHandler}
+                >
+                  ➕
+                </div>
+              )}
+
+              {isSearch && (
+                <div className="flex gap-1">
+                  <form onSubmit={submitSearchUser}>
+                    <input
+                      type="text"
+                      name="searchUser"
+                      id="searchUser"
+                      onChange={searchUserChangeHandler}
+                      value={searchUserName}
+                    />
+                  </form>
+                  <span
+                    onClick={closeSearchInputHandler}
+                    className="cursor-pointer"
+                  >
+                    ❌
+                  </span>
+
+                  <div>
+                    {searchResults?.map((user) => (
+                      <div
+                        key={user.userId}
+                        className="hover:cursor-pointer"
+                        onClick={() =>
+                          addUserHandler(user.userSeq, user.userNickname)
+                        }
+                      >
+                        {user.userNickname}
+                      </div>
+                    ))}
+                  </div>
+                </div>
+              )}
+            </div>
+          </div>
+        </div>
+
+        <div className="flex items-center">
+          <div className="w-48 text-white font-bold bg-point_purple h-full p-2 flex items-center rounded-bl-md rounded-tl-md">
+            프로젝트
+          </div>
+          <div className="flex">
+            <div className="text-white text-sm p-2">/까마귀공방</div>
+          </div>
+        </div>
       </div>
-
-      <br />
-
-      <button onClick={deleteTeamHandler}>팀 삭제</button>
-
-      <br />
-
-      <button onClick={clickTeamListHandler}>팀 목록</button>
     </div>
   );
 };
