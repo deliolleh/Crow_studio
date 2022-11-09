93c40d4013460a60484bcb5592b43ae9a2fda86f
박자연 <hosan130@yu.ac.kr>
2022-11-09 11:13:28 +0900

    S07P31D207-000 feat-be: 환경 설정 dto 수정

---
+++ goldenCrow/src/main/java/com/example/goldencrow/user/dto/SettingEditorDto.java
@@ -0,0 +1,16 @@
+package com.example.goldencrow.user.dto;
+
+import lombok.Data;
+
+@Data
+public class SettingEditorDto {
+
+    private int fontSize;
+    private String font;
+    private String theme;
+    private String autoLine;
+
+    public SettingEditorDto() {
+    }
+
+}
