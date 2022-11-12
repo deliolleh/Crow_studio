228c35d79db31c5c9c3518678c1b6429377c710f
함희주 <gkagmlwn94@naver.com>
2022-11-12 17:19:26 +0900

    feat-fe/file: projectApi에 디렉터리 리스트 보는 거 메서드 수정

--- front/src/api/projectApi.js
+++ front/src/api/projectApi.js
@@ -1,11 +1,16 @@
 import api from "./api";
 
+// const projectApi = {
+//   directoryList: (teamSeq) => api.get(`/projects/${teamSeq}`),
+//   projectCreate: (teamSeq, type, projectName) =>
+//     api.post(`/projects/${teamSeq}?type=${type}`, projectName),
+//   projectDelete: (projectData) =>
+//     api.post("/projects/projectDeleter", projectData),
+// };
+
 const projectApi = {
-  directoryList: (teamSeq) => api.get(`/projects/${teamSeq}`),
-  projectCreate: (teamSeq, type, projectName) =>
-    api.post(`/projects/${teamSeq}?type=${type}`, projectName),
-  projectDelete: (projectData) =>
-    api.post("/projects/projectDeleter", projectData),
+  directoryList: (directoryData) =>
+    api.post("/projects/directories/", directoryData),
 };
 
 export default projectApi;
--- front/src/pages/test/Test.jsx
+++ front/src/pages/test/Test.jsx
@@ -2,11 +2,35 @@ import React, { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
 
 import fileApi from "../../api/fileApi";
+import projectApi from "../../api/projectApi";
 
 import Header from "../../components/Header";
 
 const Test = () => {
   const dispatch = useDispatch();
+
+  const teamSeq = 3;
+  const type = 2;
+  const fileData = {
+    fileTitle: "test.py",
+    filePath: "/home/ubuntu/crow_data/3",
+  };
+
+  const directoryData = {
+    rootPath: `/home/ubuntu/crow_data/${teamSeq}`,
+    rootName: ``,
+  };
+
+  useEffect(() => {
+    // fileApi
+    //   .fileCreate(teamSeq, type, fileData)
+    //   .then(console.log)
+    //   .catch(console.error);
+    projectApi
+      .directoryList(directoryData)
+      .then(console.log)
+      .catch(console.error);
+  }, []);
 
   return (
     <React.Fragment>
