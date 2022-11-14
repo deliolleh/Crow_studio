600e526c11e4b7e9f3db9009b4ab212e5c7bf404
우영택 <do0134@naver.com>
2022-11-14 14:48:45 +0900

    fix-be : conflict

--- goldenCrow/src/main/java/com/example/goldencrow/liveCode/WebSocketConfig.java
+++ goldenCrow/src/main/java/com/example/goldencrow/liveCode/WebSocketConfig.java
@@ -18,7 +18,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/share/").setAllowedOriginPatterns("*").withSockJS();
+        registry.addEndpoint("/share").setAllowedOriginPatterns("http://localhost:8080").withSockJS();
     }
 
 }
