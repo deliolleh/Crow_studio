a060d35e2ed6b26a199720480e81cde0952fb09f
함희주 <gkagmlwn94@naver.com>
2022-11-14 14:33:30 +0900

    feat-fe/intro: 팀 생성하기 버튼으로 변경, 로그인 상태에 따라 로직 변경

--- front/src/pages/intro/Intro.jsx
+++ front/src/pages/intro/Intro.jsx
@@ -1,11 +1,20 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 
 import Header from "../../components/Header";
 
 const Intro = () => {
   const navigate = useNavigate();
-  const clickButtonHandler = () => navigate("/project/create");
+  const isLoggedIn = useSelector((state) => state.user.value.isLoggedIn);
+  const createTeamHandler = () => {
+    if (isLoggedIn) {
+      navigate("/teams/create");
+    } else {
+      alert("로그인 필요");
+      navigate("/login");
+    }
+  };
 
   return (
     <React.Fragment>
@@ -13,17 +22,19 @@ const Intro = () => {
         <Header />
         <div className="flex flex-col h-screen justify-center justify-items-center items-center overflow-auto">
           <div className="text-center">logo</div>
-          <div className="text-7xl font-bold text-center text-white mt-5">까마귀공방</div>
+          <div className="text-7xl font-bold text-center text-white mt-5">
+            까마귀공방
+          </div>
           <div className="text-center text-white flex-col mt-11">
             <div className="text-4xl">무언가 굉장하고 엄청난</div>
             <div className="text-4xl">웹IDE계의 이단아가 등장했다</div>
             <div className="text-4xl">다들 사용해달라 지금 당장!!</div>
           </div>
           <button
-            onClick={clickButtonHandler}
+            onClick={createTeamHandler}
             className="w-72 h-12 mt-14 text-xl font-bold bg-point_light_yellow text-component_dark hover:bg-point_yellow rounded-md transition"
           >
-            프로젝트 생성하기
+            팀 생성하기
           </button>
         </div>
       </div>
