bbae97e7bc29224bb02c45422efc72f25324627d
함희주 <gkagmlwn94@naver.com>
2022-11-16 22:59:57 +0900

    refactor-fe/user: EmailForm, PasswordForm, LoginButton 컴포넌트 분리

--- front/src/pages/login/LoginForm.jsx
+++ front/src/pages/login/LoginForm.jsx
@@ -1,5 +1,9 @@
 import React, { useState } from "react";
 
+import EmailForm from "./components/EmailForm";
+import PasswordForm from "./components/PasswordForm";
+import LoginButton from "./components/LoginButton";
+
 const emailRegEx =
   /^[A-Za-z0-9]([-_.]?[A-Za-z0-9])*@[A-Za-z0-9]([-_.]?[A-Za-z0-9])*\.[A-Za-z]{2,3}$/i;
 // const passwordRegEx = /^[A-Za-z0-9]{8,20}$/;
@@ -45,49 +49,21 @@ const LoginForm = ({ onLogin }) => {
       className="flex flex-col items-center"
     >
       {/* 이메일 */}
-      <div className="w-80 mb-1">
-        <label htmlFor="email">이메일</label>
-        <input
-          type="email"
-          id="email"
-          name="email"
-          className="mt-1 w-full text-component_dark py-2 px-3 placeholder:text-gray-300 placeholder:text-sm rounded-md transition"
-          placeholder="이메일을 입력하세요"
-          required
-          value={email}
-          onChange={emailChangeHandler}
-        />
-        <div className="h-6 mt-1 ml-3 mb-0.5 text-sm text-point_pink">
-          {emailErrMsg}
-        </div>
-      </div>
+      <EmailForm
+        email={email}
+        emailChangeHandler={emailChangeHandler}
+        emailErrMsg={emailErrMsg}
+      />
 
       {/* 비밀번호 */}
-      <div className="w-80 mb-10">
-        <label htmlFor="password">비밀번호</label>
-        <input
-          type="password"
-          id="password"
-          name="password"
-          className="mt-1 w-full text-component_dark py-2 px-3 placeholder:text-gray-300 placeholder:text-sm rounded-md transition"
-          placeholder="비밀번호를 입력하세요"
-          required
-          value={password}
-          onChange={passwordChangeHandler}
-        />
-        <div className="h-6 mt-1 ml-3 mb-0.5 text-sm text-point_pink">
-          {passwordErrMsg}
-        </div>
-      </div>
+      <PasswordForm
+        password={password}
+        passwordChangeHandler={passwordChangeHandler}
+        passwordErrMsg={passwordErrMsg}
+      />
 
-      {/* Submit Button */}
-      <button
-        type="submit"
-        className="w-80 text-lg font-bold text-component_dark bg-point_light_yellow hover:bg-point_yellow py-2 px-6 rounded-md transition mb-4"
-        onClick={submitLoginHandler}
-      >
-        로그인
-      </button>
+      {/* 로그인 버튼 */}
+      <LoginButton submitLoginHandler={submitLoginHandler}>로그인</LoginButton>
     </form>
   );
 };
