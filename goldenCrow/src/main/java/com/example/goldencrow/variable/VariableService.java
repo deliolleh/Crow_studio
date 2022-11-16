4ad9ad69b017f6aa3165e31f8482cc891975353a
이주영 <ph2978@naver.com>
2022-11-16 23:45:52 +0900

    refact-be : variable refactoring

--- goldenCrow/src/main/java/com/example/goldencrow/variable/VariableService.java
+++ goldenCrow/src/main/java/com/example/goldencrow/variable/VariableService.java
@@ -12,6 +12,9 @@ import org.springframework.web.util.UriComponentsBuilder;
 import java.net.URI;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
+
+import static com.example.goldencrow.common.Constants.*;
 
 @Service
 public class VariableService {
@@ -20,90 +23,121 @@ public class VariableService {
     String clientId = "p3IEa7WNGODfNQkwb1z2";
     String clientSecret = "MSAQnEbctM";
 
-    public HashMap<String, Object> variableRecommend(String text) {
-//        System.out.println(text);
-        HashMap<String, Object> result = new HashMap<>();
+    /**
+     * 변수명 추천 내부 로직
+     *
+     * @param data  변수명을 추천받을 한글 단어
+     * @return 추천받은 변수명 리스트 반환, 성패에 따른 result 반환
+     */
+
+    public Map<String, Object> variableRecommend(String data) {
+        Map<String, Object> serviceRes = new HashMap<>();
+        /*
+         * words : 다양한 api를 활용해 단어를 번역하여 담기위한 리스트
+         * 현재는 papago api만을 활용하고 있다
+         * */
         ArrayList<String> words = new ArrayList<>();
-
-        String papago = papagoApi(text);
-        words.add(papago);
-//        String google =
-
+        // papago api를 활용해 번역
+        String papagoWord = papagoApi(data);
+        if (papagoWord.equals(NO_SUCH)) {
+            serviceRes.put("result", NO_SUCH);
+            return serviceRes;
+        }
+        words.add(papagoWord);
+        // camel, pascal, snake 타입으로 변경된 단어들을 저장할 리스트
         ArrayList<String> converts = new ArrayList<>();
 
         for (String word : words) {
             String[] letters = word.split(" ");
-            int len = letters.length;
-            int i;
             StringBuilder camel = new StringBuilder();
             StringBuilder pascal = new StringBuilder();
             StringBuilder snake = new StringBuilder();
-            for (i = 0; i < len; i++) {
-                String clearLetter = letters[i].replaceAll("%20", "");
+            for (String letter : letters) {
+                String clearLetter = letter.replaceAll("%20", "");
                 // toLowerCase().equals => equalsIgnoreCase()로 메서드 사용 최소화
                 if (!clearLetter.equalsIgnoreCase("a") && !clearLetter.equalsIgnoreCase("an")) {
-                    // camel
+                    /* camel 타입 변수 생성
+                       첫번째 단어일 경우 첫번째 알파벳은 소문자, 나머지 알파벳은 소문자
+                       첫번째 단어가 아닐 경우 첫번째 알파벳은 대문자, 나머지 알파벳은 소문자
+                     */
                     if (camel.length() == 0) {
-                        camel.append(letters[i].toLowerCase());
+                        camel.append(letter.toLowerCase());
                     } else {
-                        camel.append(letters[i].substring(0, 1).toUpperCase()).append(letters[i].substring(1));
+                        camel.append(letter.substring(0, 1).toUpperCase())
+                                .append(letter.substring(1));
                     }
-
-                    // pascal
+                    /* pascal 타입 변수 생성
+                       첫번째 단어일 경우 첫번째 알파벳은 대문자, 나머지 알파벳은 소문자
+                       첫번째 단어가 아닐 경우 첫번째 알파벳은 대문자, 나머지 알파벳은 소문자
+                     */
                     if (pascal.length() == 0) {
-                        pascal.append(letters[i].substring(0, 1).toUpperCase()).append(letters[i].substring(1).toLowerCase());
+                        pascal.append(letter.substring(0, 1).toUpperCase())
+                                .append(letter.substring(1).toLowerCase());
                     } else {
-                        pascal.append(letters[i].substring(0, 1).toUpperCase()).append(letters[i].substring(1));
+                        pascal.append(letter.substring(0, 1).toUpperCase())
+                                .append(letter.substring(1));
                     }
-
-                    // snake
+                    /* snake 타입 변수 생성
+                       첫번째 단어일 경우 모든 알파벳은 소문자
+                       첫번째 단어가 아닐 경우 앞에 "_" 넣고 모든 알파벳은 소문자
+                     */
                     if (snake.length() == 0) {
-                        snake.append(letters[i].toLowerCase());
+                        snake.append(letter.toLowerCase());
                     } else {
-                        snake.append("_").append(letters[i].toLowerCase());
+                        snake.append("_").append(letter.toLowerCase());
                     }
                 }
             }
-
             converts.add(camel.toString());
             converts.add(pascal.toString());
             converts.add(snake.toString());
-            System.out.println(camel + " " + pascal + " " + snake);
-//            String word2 = word.replace("[a,e,i,o,u, A, E, I, O, U]", "");
-
         }
-
-        result.put("data", converts);
-        return result;
+        serviceRes.put("result", SUCCESS);
+        serviceRes.put("data", converts);
+        return serviceRes;
     }
 
+    /**
+     * 파파고 API 사용하는 내부 로직
+     *
+     * @param word  번역에 사용할 단어(한글)
+     * @return 성공 시 번역한 단어 반환, 실패 시 실패코드 반환
+     */
     public String papagoApi(String word) {
-        String response = "";
-
-        URI uri = UriComponentsBuilder.fromUriString("https://openapi.naver.com").path("/v1/papago/n2mt").queryParam("source", "ko").queryParam("target", "en").queryParam("text", word).build().toUri();
-
-
+        String serviceRes = "";
+        // papago api에 활용할 uri 생성
+        URI uri = UriComponentsBuilder.fromUriString("https://openapi.naver.com")
+                .path("/v1/papago/n2mt")
+                .queryParam("source", "ko")
+                .queryParam("target", "en")
+                .queryParam("text", word).build().toUri();
+        // naver header 생성
         RequestEntity<Void> requestEntity = naverHeader(uri);
 
-        ResponseEntity<String> result = restTemplate.exchange(uri.toString(), HttpMethod.POST, requestEntity, String.class);
+        ResponseEntity<String> restTemplateResult
+                = restTemplate.exchange(uri.toString(), HttpMethod.POST, requestEntity, String.class);
 
         try {
             JSONParser jsonParser = new JSONParser();
-            JSONObject jsonObject = (JSONObject) jsonParser.parse(result.getBody());
-            JSONObject jsonObject1 = (JSONObject) jsonObject.get("message");
-            JSONObject jsonObject2 = (JSONObject) jsonObject1.get("result");
-            response = jsonObject2.get("translatedText").toString();
+            JSONObject jsonObject = (JSONObject) jsonParser.parse(restTemplateResult.getBody());
+            JSONObject message = (JSONObject) jsonObject.get("message");
+            JSONObject result = (JSONObject) message.get("result");
+            serviceRes = result.get("translatedText").toString();
         } catch (Exception e) {
-            e.printStackTrace();
+            return UNKNOWN;
         }
-
-        System.out.println(response);
-        return response;
-
+        return serviceRes;
     }
 
+    /**
+     * naver Header를 생성하는 내부 로직
+     *
+     * @param uri   naver Header를 생성하기위해 필요한 URI
+     * @return naver Header을 생성해 RequestEntity<Void>로 반환
+     */
     private RequestEntity<Void> naverHeader(URI uri) {
-        return RequestEntity.get(uri).header("X-Naver-Client-Id", clientId).header("X-Naver-Client-Secret", clientSecret).build();
+        return RequestEntity.get(uri).header("X-Naver-Client-Id", clientId)
+                .header("X-Naver-Client-Secret", clientSecret).build();
     }
 
 //    public String googleApi(String word) {
