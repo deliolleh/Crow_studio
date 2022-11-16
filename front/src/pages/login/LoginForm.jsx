d62130270330d46c0b7c4811f96e261dfdf769dd
함희주 <gkagmlwn94@naver.com>
2022-11-16 22:06:50 +0900

    refactor-fe/user: LoginForm 리팩토링. state 분화

--- front/src/pages/login/LoginForm.jsx
+++ front/src/pages/login/LoginForm.jsx
@@ -1,64 +1,47 @@
 import React, { useState } from "react";
 
-const initialInputState = { email: "", password: "" }; // 초기 이메일, 비밀번호 상태
-const initialErrState = { emailErrMsg: "", passwordErrMsg: "" };
-
 const emailRegEx =
   /^[A-Za-z0-9]([-_.]?[A-Za-z0-9])*@[A-Za-z0-9]([-_.]?[A-Za-z0-9])*\.[A-Za-z]{2,3}$/i;
 // const passwordRegEx = /^[A-Za-z0-9]{8,20}$/;
 
 const LoginForm = ({ onLogin }) => {
-  const [inputs, setInputs] = useState(initialInputState); // 초기 입력
-  const [errMsgs, setErrMsgs] = useState(initialErrState); // 초기 에러메시지
-  const { email, password } = inputs; // 이메일, 비밀번호 상태 할당
-  const { emailErrMsg, passwordErrMsg } = errMsgs; // 에러메시지 상태 할당
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [emailErrMsg, setEmailErrMsg] = useState("");
+  const [passwordErrMsg, setPasswordErrMsg] = useState("");
 
-  const inputChangeHandler = (e) => {
-    if (e.target.name === "email") {
-      setInputs((prev) => {
-        return { ...prev, email: e.target.value };
-      });
-    } else if (e.target.name === "password") {
-      setInputs((prev) => {
-        return { ...prev, password: e.target.value };
-      });
-    }
-  };
+  const emailChangeHandler = (e) => setEmail(e.target.value);
+  const passwordChangeHandler = (e) => setPassword(e.target.value);
 
-  const submitHandler = (e) => {
+  const submitLoginHandler = (e) => {
     e.preventDefault();
     let isInvalid = false;
-    setErrMsgs(initialErrState);
-    // 이메일, 비밀번호 유효성 검사
+    setEmailErrMsg("");
+    setPasswordErrMsg("");
     if (email.trim().length === 0) {
-      setErrMsgs((prev) => {
-        return { ...prev, emailErrMsg: "이메일을 입력하세요" };
-      });
+      setEmailErrMsg("이메일을 입력하세요");
       isInvalid = true;
     } else if (!emailRegEx.test(email)) {
-      setErrMsgs((prev) => {
-        return { ...prev, emailErrMsg: "이메일 형식이 올바르지 않습니다" };
-      });
+      setEmailErrMsg("이메일 형식이 올바르지 않습니다");
       isInvalid = true;
     }
     if (password.trim().length === 0) {
-      setErrMsgs((prev) => {
-        return { ...prev, passwordErrMsg: "비밀번호를 입력하세요" };
-      });
+      setPasswordErrMsg("비밀번호를 입력하세요");
       isInvalid = true;
     }
     if (isInvalid) {
       return;
     }
+    setEmailErrMsg("");
+    setPasswordErrMsg("");
     const loginData = { userId: email, userPassword: password };
-    setErrMsgs(initialErrState);
     onLogin(loginData);
   };
 
   return (
     <form
       method="post"
-      onSubmit={submitHandler}
+      onSubmit={submitLoginHandler}
       className="flex flex-col items-center"
     >
       {/* 이메일 */}
@@ -72,7 +55,7 @@ const LoginForm = ({ onLogin }) => {
           placeholder="이메일을 입력하세요"
           required
           value={email}
-          onChange={inputChangeHandler}
+          onChange={emailChangeHandler}
         />
         <div className="h-6 mt-1 ml-3 mb-0.5 text-sm text-point_pink">
           {emailErrMsg}
@@ -90,7 +73,7 @@ const LoginForm = ({ onLogin }) => {
           placeholder="비밀번호를 입력하세요"
           required
           value={password}
-          onChange={inputChangeHandler}
+          onChange={passwordChangeHandler}
         />
         <div className="h-6 mt-1 ml-3 mb-0.5 text-sm text-point_pink">
           {passwordErrMsg}
@@ -101,7 +84,7 @@ const LoginForm = ({ onLogin }) => {
       <button
         type="submit"
         className="w-80 text-lg font-bold text-component_dark bg-point_light_yellow hover:bg-point_yellow py-2 px-6 rounded-md transition mb-4"
-        onClick={submitHandler}
+        onClick={submitLoginHandler}
       >
         로그인
       </button>
