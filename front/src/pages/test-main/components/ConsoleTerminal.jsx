559f1d7b66c882bf3efbb1224b3cd56c9574247a
함희주 <gkagmlwn94@naver.com>
2022-11-17 14:56:37 +0900

    feat-fe/console: 컴파일 응답 성공

--- front/src/pages/test-main/components/ConsoleTerminal.jsx
+++ front/src/pages/test-main/components/ConsoleTerminal.jsx
@@ -52,21 +52,17 @@ const ConsoleTerminal = (props) => {
   const compileStart = () => {
     const curPathSplit = curPath.split("/");
 
-    console.log(
-      "compileStart curPath:",
-      curPathSplit[curPathSplit.length - 2] +
-        curPathSplit[curPathSplit.length - 1]
-    );
+    console.log("compileStart curPath:", "66/wowhello/wowhello.py");
     const body = {
       type: "pure Python",
-      filePath: curPath,
+      filePath: "66/wowhello/wowhello.py",
       input: "",
     };
     compileApi
       .compilePython(body)
       .then((res) => {
         console.log(res.data);
-        setOutputData(res.data);
+        setOutputData(res.data.result);
       })
       .catch((err) => console.error(err));
   };
