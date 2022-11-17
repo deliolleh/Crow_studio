569ee4aebfe1e453072d67838bdae8afa858c625
함희주 <gkagmlwn94@naver.com>
2022-11-17 13:30:11 +0900

    feat-fe/formatcompile: 깃랩느려

--- front/src/pages/test-main/components/ConsoleTerminal.jsx
+++ front/src/pages/test-main/components/ConsoleTerminal.jsx
@@ -1,13 +1,7 @@
-// import * as React from "react";
-import React, { useState, useEffect, useRef } from "react";
-import Editor from "@monaco-editor/react";
-import files from "./files";
-import ReactTerminal from "react-terminal-component";
-import { EmulatorState, FileSystem } from "javascript-terminal";
-import editorApi from "../../../../api/editorApi";
-import compileApi from "../../../../api/compileApi";
+import React, { useState } from "react";
+import editorApi from "../../../api/editorApi";
+import compileApi from "../../../api/compileApi";
 
-// icons
 import { BsPlayFill } from "react-icons/bs";
 import { BsStopFill } from "react-icons/bs";
 import { TbTerminal } from "react-icons/tb";
@@ -17,49 +11,15 @@ export const MakeEditorData = (
   titlePrefix = "Tab",
   useTitleCounter = true
 ) => {
-  // 에디터 파일
-  const editorRef = useRef(null);
   const [fileName, setFileName] = useState("script.js");
   const [language, setLanguage] = useState("python");
 
-  const file = files[fileName];
-
-  const data = [];
-
-  useEffect(() => {
-    editorRef.current?.focus();
-  }, [file.name]);
-
-  // 포맷팅 영역
-  const format = async () => {
-    // When formatting Button Click, useRef of that part's value get
-    const sendCode = editorRef.current.getValue();
-    const body = {
-      text: sendCode,
-    };
-    // console.log(body);
-    await editorApi.formatPut(language, body).then((res) => {
-      const number = res.data.data;
-      console.log("dataCome ", res.data.data);
-      const body2 = {
-        name: number,
-      };
-      editorApi
-        .formatGet(language, body2)
-        .then((res) => {
-          console.log("updateCode: ", res.data.data);
-          const summaryCode = res.data.data.trim();
-          editorRef.current.getModel().setValue(summaryCode);
-        })
-        .catch((err) => console.error(err));
-    });
-  };
-
   // 린트 영역
   const lint = () => {
-    const sendCode = editorRef.current.getValue();
+    // const sendCode = editorRef.current.getValue();
     const body = {
-      text: sendCode,
+      // text: sendCode,
+      text: "",
     };
     editorApi
       .lint(language, body)
@@ -76,99 +36,11 @@ export const MakeEditorData = (
         lintResult.sort();
         // console.log("lineResult: ", lintResult);
       })
-      .catch((err) => {
-        console.error(err);
-      });
+      .catch(console.error);
   };
-
-  for (let i = 0; i < number; i++) {
-    data.push({
-      title: useTitleCounter ? `${titlePrefix} ${i + 1}` : titlePrefix,
-      content: (
-        <div>
-          <button onClick={format}>포맷팅</button>
-          <button onClick={lint}>린트</button>
-          <Editor
-            style={{
-              resize: "vertical",
-              overflow: "auto",
-            }}
-            height="200px"
-            // height="calc(100vh - 31px)"
-            theme="vs-dark"
-            // path={file.name}
-            path={i + 1 === 1 ? "script.js" : "style.css"}
-            // defaultLanguage={file.language}
-            defaultLanguage={language}
-            // defaultValue={file.value}
-            // defaultValue={
-            //   i + 1 === 1 ? files["script.js"].value : files["style.css"].value
-            // }
-            defaultValue={""}
-            onMount={(editor) => (editorRef.current = editor)}
-            options={{
-              scrollBeyondLastLine: false,
-              fontSize: "14px",
-              fontFamily: "JetBrains Mono",
-              automaticLayout: true,
-            }}
-          />
-        </div>
-      ),
-    });
-  }
-  return data;
 };
 
-export const MakeConsoleData = (
-  number,
-  titlePrefix = "Tab",
-  useTitleCounter = true
-) => {
-  const data = [];
-
-  const customState = EmulatorState.create({
-    fs: FileSystem.create({
-      "/home": {},
-      "/home/README": { content: "This is a text file" },
-      "/home/nested/directory": {},
-      "/home/nested/directory/file": { content: "End of nested directory!" },
-    }),
-  });
-
-  for (let i = 0; i < number; i++) {
-    data.push({
-      title: useTitleCounter ? `${titlePrefix} ${i + 1}` : titlePrefix,
-      content: (
-        <div>
-          {/* <b>Content {i + 1}</b>
-          <p>{lorem.generateWords(15)}</p> */}
-          <ReactTerminal
-            style={{ overflow: "hidden" }}
-            promptSymbol="🔥>"
-            theme={{
-              background: "#333333",
-              promptSymbolColor: "#6effe6",
-              commandColor: "#fcfcfc",
-              outputColor: "#fcfcfc",
-              errorOutputColor: "#ff89bd",
-              fontSize: "14px",
-              spacing: "1.5%",
-              width: "100%",
-              // height: "calc(100vh - 551.5px)",
-              height: "calc(100vh - 50px)",
-              // height: "100%",
-            }}
-            emulatorState={customState}
-          />
-        </div>
-      ),
-    });
-  }
-  return data;
-};
-
-export const CompileEditor = ({ consoleHeight }) => {
+const ConsoleTerminal = () => {
   const [inputData, setInputData] = useState("");
   const [outputData, setOutputData] = useState("");
   const [selectOption, setSelectOption] = useState([]);
@@ -291,3 +163,5 @@ export const CompileEditor = ({ consoleHeight }) => {
     </div>
   );
 };
+
+export default ConsoleTerminal;
