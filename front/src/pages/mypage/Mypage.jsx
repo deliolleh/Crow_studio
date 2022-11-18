b732a8c1c1b0a2b668ca14dd25e9795af3954501
김수빈 <foursoob@naver.com>
2022-11-18 15:40:13 +0900

    Merge from feat-fe/scroll_css into dev-front

--- front/src/pages/mypage/Mypage.jsx
+++ front/src/pages/mypage/Mypage.jsx
@@ -11,9 +11,9 @@ const Mypage = () => {
   const mySeq = useSelector((state) => state.user.value.mySeq);
 
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col h-full w-full">
       <Header />
-      <section className="flex md:flex-row flex-col w-screen h-screen justify-center items-center m-3 mb-6">
+      <section className="flex md:flex-row flex-col h-full justify-center items-center m-3 mb-6">
         {/* 프로필 */}
         <Profile userSeq={userSeq} mySeq={mySeq} />
         {/* 회원정보수정 */}
