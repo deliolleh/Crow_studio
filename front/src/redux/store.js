10f6f324f26357001f146dd74f3d1aef338b82cb
함희주 <gkagmlwn94@naver.com>
2022-11-20 20:32:19 +0900

    feat-fe/loading: globalSlice의 Reducer 적용

--- front/src/redux/store.js
+++ front/src/redux/store.js
@@ -2,10 +2,12 @@ import { configureStore } from "@reduxjs/toolkit";
 
 import userReducer from "./userSlice";
 import teamReducer from "./teamSlice";
+import globalReducer from "./globalSlice";
 
 export const store = configureStore({
   reducer: {
     user: userReducer,
     team: teamReducer,
+    global: globalReducer,
   },
 });
