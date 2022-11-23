f0469dde5d227c12237b4c746a6bf78d2aef428d
이주영 <ph2978@naver.com>
2022-11-24 00:04:02 +0900

    fix-be : compiler

--- goldenCrow/src/main/java/com/example/goldencrow/compile/CompileService.java
+++ goldenCrow/src/main/java/com/example/goldencrow/compile/CompileService.java
@@ -263,9 +263,10 @@ public class CompileService {
         if (type == 1) {
             String[] pythonCmd = {"docker", "logs", "-f", conAndImgName};
             String pythonResponse = resultStringService(pythonCmd);
+            String pathUpdateRes = pythonResponse.replace(BASE_URL, "")
             System.out.println(pythonResponse);
             serviceRes.put("result", SUCCESS);
-            serviceRes.put("response", pythonResponse);
+            serviceRes.put("response", pathUpdateRes);
             return serviceRes;
         } else if (portNumService(conAndImgName).equals(port)) {
             serviceRes.put("result", SUCCESS);
