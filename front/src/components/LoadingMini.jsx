adca5115623252621f3410c10d676a67471d04d4
foursoob <foursoob@naver.com>
2022-11-24 02:04:41 +0900

    fix-fe: mini loading css 수정

--- front/src/components/LoadingMini.jsx
+++ front/src/components/LoadingMini.jsx
@@ -21,13 +21,13 @@ const LoadingMini = () => {
         className="w-full h-full flex flex-col justify-center items-center bg-component_item_bg_+2_dark"
         style={{ fontFamily: "Pretendard Variable" }}
       >
-        <div className="ml-6">
+        <div className="ml-6 overflow-hidden">
           <Lottie options={defaultOptions} height={200} width={400} />
         </div>
-        <div className="text-[37px] text-point_purple font-bold">
+        <div className="md:text-[37px] text-[26px] text-point_purple font-bold md:mb-2 mb-1">
           까마귀공방
         </div>
-        <div className="text-xl text-white font-medium">
+        <div className="md:text-xl text-md text-white font-medium">
           잠시만 기다려주세요
         </div>
       </div>
