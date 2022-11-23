e85bf98e7647252b629ec3a873a0cb898c0d9c4b
이주영 <ph2978@naver.com>
2022-11-24 03:48:13 +0900

    feat-be : unlogin compile

--- goldenCrow/src/main/java/com/example/goldencrow/config/WebMvcConfig.java
+++ goldenCrow/src/main/java/com/example/goldencrow/config/WebMvcConfig.java
@@ -42,7 +42,8 @@ public class WebMvcConfig implements WebMvcConfigurer {
                 .excludePathPatterns("/api/compile/py")         // python 컴파일
                 .excludePathPatterns("/api/compile/py/stop")    // python 컴파일 중지
                 .excludePathPatterns("/api/variable")           // 변수명 추천
-                .excludePathPatterns("/api/unlogin");            // 비로그인 사용자 세션 처리
+                .excludePathPatterns("/api/unlogin")            // 비로그인 사용자 세션 처리
+                .excludePathPatterns("/api/unlogin/compile");
     }
 
     /**
